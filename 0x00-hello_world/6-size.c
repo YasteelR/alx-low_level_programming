96820865b1ce149aafa1961e254595700f0e869c
yasteel <yasteel001@gmail.com>
2022-06-02 05:47:03 -0700

    size of different types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	long int x;
+	long long int y;
+	int z;
+	char m;
+	float n;
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(m));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(z));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(x));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(y));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(n));
+	return (0);
+}
