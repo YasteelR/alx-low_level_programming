f0aea117521eff901549d5b6ddad38379e339f2b
yasteel <yasteel001@gmail.com>
2022-06-08 16:45:11 -0700

    l

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs - return the absolute value
+ * @i: value use to find the absolute
+ * Return: the absolute value.
+ */
+int _abs(int i)
+{
+	if (i < 0)
+		return (-i);
+	return (i);
+}
