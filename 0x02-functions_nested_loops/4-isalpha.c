6051cf5987f1f5f5f61c6bfcfb01c425e7e5d317
Yasteel <67509734+YasteelR@users.noreply.github.com>
2022-06-09 01:06:52 +0200

    Update 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isalpha - check if  lower case.
+ * @c: character to test
+ * Return: 1 (True) else 0 (False)
+ */
+int _isalpha(char c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	return (0);
+}
