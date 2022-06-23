e6a5a27060851497019140535b1136435075edd2
yasteel <yasteel001@gmail.com>
2022-06-23 22:26:01 +0200

    recursion

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * palind2 - finds length of of string
+ * @a: string
+ * @l: integer to count length
+ * Return: On success 1.
+ */
+int palind2(char *a, int l)
+{
+	if (*a == 0)
+		return (l - 1);
+	return (palind2(a + 1, l + 1));
+}
+/**
+ * palind3 - compares string and its inverse
+ * @a: string
+ * @l: length
+ * Return: On success 1.
+ */
+
+int palind3(char *a, int l)
+{
+	if (*a != *(a + l))
+		return (0);
+	else if (*a == 0)
+		return (1);
+	return (palind3(a + 1, l - 2));
+}
+/**
+ * is_palindrome - checks for similarities
+ * @s: string to evaluate
+ * Return: On success 1.
+ */
+int is_palindrome(char *s)
+{
+	int l;
+
+	l = palind2(s, 0);
+	return (palind3(s, l));
+}
