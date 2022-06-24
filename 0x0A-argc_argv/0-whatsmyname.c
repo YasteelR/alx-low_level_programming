4b17a4a319f43e16db002ef837e71abcb2e0c57c
yasteel <yasteel001@gmail.com>
2022-06-25 00:45:14 +0200

    argc argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -  prints name
+ * @argc: index
+ * @argv: pointer array of strings
+ * Return: 0
+ **/
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s\n", *argv);
+	return (0);
+}
