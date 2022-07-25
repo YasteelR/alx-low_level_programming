93424aa144f50a136ff5ab2e3f0d3e725d2decad
yasteel <yasteel001@gmail.com>
2022-07-25 23:51:42 +0200

    0x15. C - File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,24 +7,25 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, out, i;
+	int fd, i, j = 0;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	if (fd == -1)
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd < 0)
 		return (-1);
 
-	if (text_content != NULL)
+	if (text_content)
 	{
-		while (text_content[i])
-			i++;
-
-		out = write(fd, text_content, i);
-		if (out == -1)
+		while (text_content[j])
+			j++;
+		i = write(fd, text_content, j);
+		if (i != j)
 			return (-1);
 	}
+
 	close(fd);
+
 	return (1);
 }
